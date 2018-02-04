b6533f97d65a99f27762066e134dbaff82f1e77c
Jesse Lumarie <jesse.lumarie@gmail.com>
2018-02-04 16:24:59 +0000

    And remove some cruft

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 const setCopyrightYear = () => {
-  console.log('here')
   const currentYear = new Date().getFullYear()
   const copyrightElement = document.getElementById('copyright')
   copyrightElement.innerHTML = `&copy; ${currentYear} Jesse Lumarie`
