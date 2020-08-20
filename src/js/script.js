82b530ee7e927080d2a7efe4305e3be0d1145031
Jesse Lumarie <jesse.lumarie@gmail.com>
2020-08-19 20:27:07 -0700

    Add privacy policy and update copyright script

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,13 @@
+
 const setCopyrightYear = () => {
   const currentYear = new Date().getFullYear()
   const copyrightElement = document.getElementById('copyright')
   copyrightElement.innerHTML = `&copy; ${currentYear} Jesse Lumarie`
+}
+
+if ( document.readyState !== 'loading') {
+  setCopyrightYear()
+} else {
+  setCopyrightYear()
+  document.addEventListener("DOMContentLoaded", setCopyrightYear);
 }
